11c95667dc7dad69a4e0357b9e8836d4d116eeb2
Donald <59314046+DonaldCapodilupo@users.noreply.github.com>
2023-08-31 20:35:13 -0400

    Database_Modifier adapted so that you can check all Databases with one case number. As a result, you will need to specify specic tables if you only want specific data.

--- Backend.py
+++ Backend.py
@@ -1,5 +1,3 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, insert, inspect
-from sqlalchemy_utils import database_exists, create_database
 import os, sqlite3
 
 
@@ -22,75 +20,123 @@ def save_Admin_Form_Data(form_data, customer, report_type):
 
     data = {}
 
-
-
     os.mkdir("Admin/" + customer)
-
-
-
-
 
     for key, value in form_data.items():
         data[key] = value
 
-
-    with open("Admin/"+ customer+"/" + report_type + '.json', 'w') as f:
+    with open("Admin/" + customer + "/" + report_type + '.json', 'w') as f:
         json.dump(data, f)
 
+
 class Database_Modifier:
-    def __init__(self, table_name):
-        self.table_name = table_name
+    def __init__(self):
+        self.database_name = "Information.db"
 
-    def check_If_Table_Exists(self, list_of_columns):
-        db = sqlite3.connect("Information.db")
+    def check_If_Table_Exists(self, table_name, list_of_columns):
+        db = sqlite3.connect(self.database_name)
 
-        list_of_columns = [column.replace(" ","_") for column in list_of_columns]
+        list_of_columns = [column.replace(" ", "_").replace("-", "_") for column in list_of_columns]
+        print(list_of_columns)
 
-        create_table_sql = f"CREATE TABLE IF NOT EXISTS {self.table_name} (id INTEGER PRIMARY KEY AUTOINCREMENT  , {', '.join([f'{col} TEXT' for col in list_of_columns])})"
+        print(table_name + " IS THE TABLE NAME")
+        print(
+            f"CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY AUTOINCREMENT  , {', '.join([f'{col} TEXT' for col in list_of_columns])})"
+        )
+
+        create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY AUTOINCREMENT, {', '.join([f'{col} TEXT' for col in list_of_columns])})"
 
         db.execute(create_table_sql)
         db.commit()
 
-    def create_Database_Row(self, dict_of_values):
-        db = sqlite3.connect("Information.db")
+    def create_Database_Row(self, table_name, dict_of_values):
+        db = sqlite3.connect(self.database_name)
 
-        columns = [column_name.replace(" ","_") for column_name in dict_of_values.keys()]
-        values = [value.replace(" ","_") for value in dict_of_values.values()]
+        columns = [column_name.replace(" ", "_").replace("-", "_") for column_name in dict_of_values.keys()]
+        values = [value.replace(" ", "_").replace("-", "_") for value in dict_of_values.values()]
 
-        insert_sql = f"INSERT INTO {self.table_name} ({', '.join(columns)}) VALUES ({', '.join(['?']*len(columns))})"
+        print("Values: ")
+        for i in values:
+            print(i)
 
+        insert_sql = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({', '.join(['?'] * len(columns))})"
 
         db.execute(insert_sql, values)
         db.commit()
 
-    def read_Database_Table(self, searched_name):
+    def read_Database_Single_Table(self, table_name):
+        pass
+
+    def read_Database_All_Tables(self, case_number):
+        import sqlite3
         import pandas as pd
-        db_connection = 'sqlite+pysqlite:///Information.db'
-        engine = create_engine(db_connection)
-        conn = engine.connect()
 
-        df = pd.read_sql('SELECT * FROM ' + self.table_name, conn,
-                         )
+        try:
 
-        for index, row in df.iterrows():
-            if row["BOLO_Person_Details_Name"] == searched_name:
-                print(row)
+            # Making a connection between sqlite3
+            # database and Python Program
+            sqliteConnection = sqlite3.connect(self.database_name)
+
+            # If sqlite3 makes a connection with python
+            # program then it will print "Connected to SQLite"
+            # Otherwise it will show errors
+            print("Connected to SQLite")
+
+            # Getting all tables from sqlite_master
+            sql_query = """SELECT name FROM sqlite_master
+            WHERE type='table';"""
+
+            # Creating cursor object using connection object
+            cursor = sqliteConnection.cursor()
+
+            # executing our sql query
+            list_of_table_names = [table_name[0] for table_name in cursor.execute(sql_query).fetchall()]
+
+            test_names = {case_number: {}}
+
+            for database_table in list_of_table_names:
+                df = pd.read_sql('SELECT * FROM ' + database_table, sqliteConnection)
+
+                if str(database_table) != "sqlite_sequence" :
+                    test_names[case_number][database_table] = {}
+
+                for index, row in df.iterrows():
+                    if row.get(key="Case_Number") == case_number:
+
+                        test_names[case_number][database_table][
+                            str(row["Basic_Details_First_Name"] + " " + row["Basic_Details_Last_Name"])] = {}
+                        for data_name, data_value in row.items():
+                            print("data_name: " + str(data_name))
+                            print("data_value: " + str(data_value))
+
+                            test_names[case_number][database_table][str(row["Basic_Details_First_Name"] + " " + row["Basic_Details_Last_Name"])][str(data_name)] = str(data_value)
+
+        except AttributeError as error:
+            print("Failed to execute the above query", error)
+
+        finally:
+            # Inside Finally Block, If connection is
+            # open, we need to close it
+            if sqliteConnection:
+                # using close() method, we will close
+                # the connection
+                sqliteConnection.close()
+
+                # After closing connection object, we
+                # will print "the sqlite connection is
+                # closed"
+                print("the sqlite connection is closed")
 
     def update_Database_Table(self):
         pass
 
-    def destroy_Database_Row(self, id_number):
-        db = sqlite3.connect("Information.db")
+    def destroy_Database_Row(self, table_name, id_number):
+        db = sqlite3.connect(self.database_name)
 
-        insert_sql = f"DELETE FROM {self.table_name} WHERE id = '{id_number}'"
-
-
+        insert_sql = f"DELETE FROM {table_name} WHERE id = '{id_number}'"
 
         db.execute(insert_sql)
         db.commit()
 
 
-
-
-
-
+#Database_Modifier("Information.db").read_Database_All_Tables("AB1234")
