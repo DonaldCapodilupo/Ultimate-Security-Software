2b935d981a6dba3984067d5ee42e9f864c7fcab7
Donald <59314046+DonaldCapodilupo@users.noreply.github.com>
2023-11-23 20:58:40 -0500

    Added some new functions to use on the backend. May add functionallity to frontend.

--- Backend.py
+++ Backend.py
@@ -48,8 +48,196 @@ def create_License_Plate_Image(plate_num):
     f.write(data)
     f.close()
 
+def license_Plate_Capture():
+    import pygetwindow as gw
+
+    win = gw.getWindowsWithTitle('Fullscreen')[0] #OBS 30.0.0
+    win.activate()
+
+    number_of_current_images = len(os.listdir("Plates"))
+
+    while len(os.listdir('Plates')) == number_of_current_images:
+        continue
+
+    win = gw.getWindowsWithTitle('Ultimate-Security-Software')[0]
+    win.activate()
 
 
+
+#license_Plate_Capture()
+
+def coordinates_To_Address(lat_long_string):
+    if len(lat_long_string) < 15:
+        return
+    import requests, json
+    api_key = "AIzaSyAnRJvIU0LM1tPQCKgBZntZJn04ukyvp5o"
+
+    google_JSON_url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat_long_string.replace(" ","") + "&key=" + api_key
+
+    cat = json.loads(requests.get(google_JSON_url).text)
+    print(cat)
+
+
+    street_number = cat['results'][0]["formatted_address"]
+    town_state_zip = cat['results'][3]["formatted_address"]
+    county = cat['results'][5]["formatted_address"]
+
+    print(street_number)
+    print(town_state_zip)
+    print(county)
+
+    print("Visual: ")
+    print("http://maps.google.com/maps?q=&layer=c&cbll="+ lat_long_string.replace(" ","") + "&cbp=11,0,0,0,0")
+
+    return [street_number, town_state_zip, county, lat_long_string]
+
+
+
+
+#API KEY = AIzaSyAnRJvIU0LM1tPQCKgBZntZJn04ukyvp5o
+def record_Geographic_Coordinates():
+    import pyperclip, csv, time
+
+    pyperclip.waitForNewPaste(10)
+
+    return_list = []
+
+    loop_bool = True
+
+    while loop_bool:
+        try:
+            current_clipboard = pyperclip.paste()
+            print("Copying: " + current_clipboard)
+
+
+            return_list.append(current_clipboard)
+
+            print("Appending: " + pyperclip.paste())
+
+
+            pyperclip.waitForNewPaste(10)
+        except pyperclip.PyperclipTimeoutException:
+            print("Complete")
+
+            loop_bool = False
+
+    print(return_list)
+
+
+
+    with open("output2.csv", "a", newline="\n") as fp:
+        wr = csv.writer(fp, dialect='excel')
+        print("Return List")
+        print(return_list)
+        for item in return_list:
+            print("item")
+            print(item)
+
+
+            wr.writerow(coordinates_To_Address(item))
+
+
+
+    lat_long_string = "42.53450351546361,-71.25236935235654"
+
+
+
+
+
+
+
+        #if pyperclip.paste() == current_clipboard:
+        #    pyperclip.waitForPaste()
+        #    current_clipboard = pyperclip.paste()
+        #else:
+        #    print("Current Clipboard: " + current_clipboard)
+        #
+        #    pyperclip.copy(None)
+        #    while pyperclip.paste() is None:
+        #
+        #        print(pyperclip.paste())
+
+
+
+
+
+#record_Geographic_Coordinates()
+
+#Function to scrape property owner information.
+#Need to find way to look up specific addresses
+def patriotProperties_Scraper():
+    from bs4 import BeautifulSoup
+    import requests
+
+    print(requests.get("https://billerica.patriotproperties.com/Summary.asp?AccountNumber=12544").text)
+
+    data_dict = {}
+    with open("patriots template.html") as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+
+    cat = []
+    for table in soup.find_all('td'):
+        #while index
+        if table.string not in [None, "\n","\t",""," "]:
+            relevant_text = table.text.replace("\
+                        "," ").strip()
+            print(relevant_text)
+            cat.append(relevant_text)
+
+    return_dict = {}
+    print(cat)
+
+#
+def scrape_Arrest_Log(pdf_file):
+    from PyPDF2 import PdfReader
+    import json
+
+    reader = PdfReader(pdf_file)
+
+    page = reader.pages[0].extract_text().split("\n")
+
+    loop_dict = {"Name": "",
+                 "Age": "",
+                 "Offense": "",
+                 "Gender": "",
+                 "Date of Arrest": "",
+                 "Address": ""}
+    return_list = []
+
+    for item in page:
+
+        if "Name:" in item:
+            offense_text = item[: item.index("Name:")].replace("   "," ").replace("_","")
+
+            name_text = item[item.index("Name:"): item.index("Age:")].replace("   "," ")[6:].strip()
+
+            age_text = item[item.index("Age:"):item.index("Sex:")].strip()[5:]
+
+
+            gender_text = item[item.index("Sex:"):][5:]
+
+            loop_dict["Name"] = name_text
+            loop_dict["Age"] = age_text
+            loop_dict["Offense"] = offense_text
+            loop_dict["Gender"] = gender_text
+
+        elif "Date of Arrest" in item:
+            date_of_arrest = item[17:]
+
+            loop_dict["Date of Arrest"] = date_of_arrest
+
+        elif "Address" in item:
+            address = item[10:]
+            loop_dict["Address"] = address
+
+        elif item == "Charged with: ":
+            return_list.append(dict(loop_dict))
+
+
+    return return_list
+
+
+#patriotproperties_Scraper()
 class Database_Modifier:
     def __init__(self):
         self.database_name = "Information.db"
@@ -169,6 +357,20 @@ class Database_Modifier:
         db.execute(insert_sql)
         db.commit()
 
+
+def arrest_Log_Loop():
+
+    test_loop = []
+    for directory in os.listdir("Forms/Billerica PD Arrest Records"):
+        test_loop.append(scrape_Arrest_Log("Forms/Billerica PD Arrest Records/" + directory))
+
+    for val1 in test_loop:
+        for val2 in val1:
+            Database_Modifier().check_If_Table_Exists("Arrest_Records", val2.keys())
+            Database_Modifier().create_Database_Row("Arrest_Records", val2)
+            print(val2)
+
+arrest_Log_Loop()
 
 #print(Database_Modifier().read_Database_All_Tables("AB1234"))
 #print(Database_Modifier().read_Database_Single_Table("People_Of_Interest", "AB1234"))
