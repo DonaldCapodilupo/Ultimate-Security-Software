fa43e6db87a8b3a70b75a6d5c4e9f20610e28022
Donald <59314046+DonaldCapodilupo@users.noreply.github.com>
2024-01-21 07:50:07 -0500

    Started working on check case status. Added House-Profile and Plate-Capture

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-#Generic Functions
+# Generic Functions
 @app.route('/', methods=["POST", "GET"])
 def main_Menu():
     if request.method == "POST":
@@ -19,6 +19,7 @@ def main_Menu():
             "Person of Interest": "create_Person_Of_Interest_Report",
             "Witness Statement": "create_Witness_Statement_Report",
             "Create BOLO": "create_BOLO",
+            "Capture Plate":"capture_Plate_Image"
         }
 
         reports_that_require_case_numbers = ["Daily Activity Report", "Check Case Status", "Witness Statement",
@@ -37,7 +38,7 @@ def main_Menu():
         case_numbers = backend_data["Case_Number"].to_list()
 
         data_dict = {
-            "Case Number":[case_number.replace("_","-") for case_number in case_numbers]
+            "Case Number": [case_number.replace("_", "-") for case_number in case_numbers]
         }
 
         return render_template("main.html", infoDict=data_dict)
@@ -50,62 +51,60 @@ def read_Case_Status(case_number):
     else:
         from Backend import Database_Modifier
 
-        #Right now, just pulling one specific user for proof of concept.
-        #Need a way to go "Okay user wants this case number" then search all tables for anything related.
+        # Right now, just pulling one specific user for proof of concept.
+        # Need a way to go "Okay user wants this case number" then search all tables for anything related.
 
         case_reports = Database_Modifier().read_Database_Single_Table("New_Case")
 
+        col_headers = [col for col in case_reports.columns]
+        print(col_headers)
 
+        print(case_reports.head())
+        specific_case_report = case_reports.loc[case_reports["Case_Number"] == case_number.replace("-", "_")]
 
-        #for  row in case_report.items():
-        #    print(row[0])
+        return_Dict_New_Case = {
 
-        specific_case_report_data = case_reports.loc[case_reports['Case_Number'] == case_number.replace("-","_")].values.flatten().tolist()
+        }
 
+        # "Customer Name": specific_case_report.Customer_Name.item().replace("_", " "),
+        #    "Investigation Type": specific_case_report.Investigation_Type.item().replace("_", " "),
+        #    "Case Purpose": specific_case_report.Case_Purpose.item().replace("_", " "),
+        #    "Case Scope": specific_case_report.Case_Scope.item().replace("_", " "),
 
-        customer_name = specific_case_report_data[1].replace("_"," ").split()
-        print(customer_name)
-        print("Customer First name " + customer_name[0])
-        print("Customer Last name " + customer_name[1])
-
-
-
-        customer_data = case_reports.loc([case_reports['Case_Number'] == case_number])
-
-
-        print(first_name)
-
-        initial_report_df = Database_Modifier().read_Database_Single_Table("Initial_Customer_Report")
-        case_report_info = initial_report_df.iloc[initial_report_df['First Name'] == case_number and case_report[''] == case_number].replace("_"," ")
-
-        data_dict = {}
-
-        for key, value in case_info.items():
-
-            if key == "Complainant Email":
-                break
+        for header in col_headers:
+            if header == "id" or header == "submit_button":
+                continue
             else:
-                data_dict[str(key).replace("_"," ")] = str(value).replace("_"," ")
+                return_Dict_New_Case[header.replace("_", " ")] = specific_case_report[header].item().replace("_", " ")
 
-        for key, value in data_dict.items():
-            print("Key: " + key)
-            print("Value: " + value)
+        customer_first_name = return_Dict_New_Case["Customer Name"].split(" ")[0]
+        customer_last_name = return_Dict_New_Case["Customer Name"].split(" ")[1]
 
-        return render_template("View Data/Non Admin Initial Report.html", infoDict=data_dict)
+        initial_report = Database_Modifier().read_Database_Single_Table("Initial_Customer_Report")
+
+        other_headers = [col for col in initial_report.columns]
+
+        specific_initial_report = initial_report.loc[
+            initial_report["Complainant_Last_Name"] == customer_last_name]
+
+        for header in other_headers:
+            if header == "id" or header == "submit_button":
+                continue
+            else:
+                return_Dict_New_Case[header.replace("_", " ")] = specific_initial_report[header].item().replace("_", " ")
+
+        return render_template("View Data/View Case.html", case_number=case_number, infoDict=return_Dict_New_Case)
 
 
-#Admin Functions
+# Admin Functions
 @app.route('/Initial-Customer-Report', methods=["POST", "GET"])
 def create_Initial_Customer_Report():
     if request.method == "POST":
         from Backend import save_Admin_Form_Data, Database_Modifier
         data_dict = request.form.to_dict()
 
-
-        Database_Modifier().check_If_Table_Exists("Initial_Customer_Report",data_dict.keys())
-        Database_Modifier().create_Database_Row("Initial_Customer_Report",data_dict)
-
-
+        Database_Modifier().check_If_Table_Exists("Initial_Customer_Report", data_dict.keys())
+        Database_Modifier().create_Database_Row("Initial_Customer_Report", data_dict)
 
         return redirect(url_for("main_Menu"))
 
@@ -115,9 +114,9 @@ def create_Initial_Customer_Report():
         backend_data = Database_Modifier().read_Database_Single_Table("Employees")
         employee_names = backend_data["Name"].to_list()
 
-        data_to_fill = {"Agent Receiving":employee_names,
+        data_to_fill = {"Agent Receiving": employee_names,
                         "Case Lead Investigator": employee_names,
-                        "Investigating Agent":employee_names,
+                        "Investigating Agent": employee_names,
                         "Manager Reviewing": employee_names,
                         }
 
@@ -133,7 +132,7 @@ def create_New_Case():
         data_dict = request.form.to_dict()
 
         Database_Modifier().check_If_Table_Exists("New_Case", data_dict.keys())
-        Database_Modifier().create_Database_Row("New_Case",data_dict)
+        Database_Modifier().create_Database_Row("New_Case", data_dict)
 
         create_Case(data_dict["Case Number"])
 
@@ -153,15 +152,11 @@ def create_New_Case():
         print("Customer Names:")
         print(customer_names)
         data_to_fill = {
-            "Customer Name":customer_names
+            "Customer Name": customer_names
         }
-        #customer_names = [first_name for first_name in zip(backend_data["Complainant_First_Name"]), backend_data["Complainant_Last_Name"] ]
-
-            #zip(backend_data["Complainant_First_Name"], backend_data["Complainant_Last_Name"])
-
-        #print(customer_names)
 
         return render_template("Capture Data/New Case.html", infoDict=data_to_fill)
+
 
 @app.route('/Invoice', methods=["POST", "GET"])
 def create_Customer_Invoice():
@@ -186,7 +181,7 @@ def create_BOLO(case_number):
         return render_template("Capture Data/BOLO File.html", case_num=escape(case_number))
 
 
-#Investigator Functions
+# Investigator Functions
 @app.route('/Create-Person-Of-Interest-Report/<string:case_number>', methods=["POST", "GET"])
 def create_Person_Of_Interest_Report(case_number):
     if request.method == "POST":
@@ -199,6 +194,7 @@ def create_Person_Of_Interest_Report(case_number):
 
     else:
         return render_template("Capture Data/Person of Interest.html", case_num=case_number)
+
 
 @app.route('/Create-Interview-Report/<string:case_number>', methods=["POST", "GET"])
 def create_Interview_Report(case_number):
@@ -213,6 +209,7 @@ def create_Interview_Report(case_number):
     else:
         return render_template("Capture Data/Interview Report.html", case_num=case_number)
 
+
 @app.route('/Create-Witness-Statement-Report/<string:case_number>', methods=["POST", "GET"])
 def create_Witness_Statement_Report(case_number):
     if request.method == "POST":
@@ -225,6 +222,7 @@ def create_Witness_Statement_Report(case_number):
 
     else:
         return render_template("Capture Data/Witness Statement Form.html", case_num=case_number)
+
 
 @app.route('/Daily-Activity-Report/<string:case_number>', methods=["POST", "GET"])
 def create_Daily_Activity_Report(case_number):
@@ -240,5 +238,47 @@ def create_Daily_Activity_Report(case_number):
         return render_template("Capture Data/Daily Activity Report.html", case_num=case_number)
 
 
+@app.route('/House-Profile/', methods=["POST", "GET"])
+def read_House_Profile_Report():
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("View Data/Non Admin House Profile.html")
+
+@app.route('/Capture-Plate/', methods=["POST", "GET"])
+def capture_Plate_Image():
+    if request.method == "POST":
+        from Backend import license_Plate_Capture
+        import os,time
+        button_clicked = request.form['submit_button']
+
+        if button_clicked == "Return Home":
+            return  redirect(url_for("main_Menu"))
+        elif button_clicked == "Capture Plate":
+            license_Plate_Capture()
+            time.sleep(2)
+            return render_template("Capture Data/Capture Plate.html",image=True)
+
+        elif button_clicked == "Delete Plate":
+            print(os.getcwd())
+            os.remove("static/images/Screenshot_.png")
+            return render_template("Capture Data/Capture Plate.html",image=False)
+
+        elif button_clicked == "Save Plate":
+
+            os.remove("static/images/_Screenshot.png")
+            return render_template("Capture Data/Capture Plate.html",image=True)
+
+
+
+        else:
+
+            return render_template("Capture Data/Capture Plate.html")
+
+    else:
+        return render_template("Capture Data/Capture Plate.html")
+
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",debug=False)
